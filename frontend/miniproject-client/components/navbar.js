05dfdc5fabae6935bc3c15ff7475e0aa67db8a80
SingKup <thanawat452@gmail.com>
2021-04-25 12:11:28 +0700

    Final Edit CSS

--- frontend/miniproject-client/components/navbar.js
+++ frontend/miniproject-client/components/navbar.js
@@ -1,14 +1,16 @@
 import Link from 'next/link'
+import { IoHome,IoAccessibility,IoBagSharp,IoHappy,IoLockClosed,IoNewspaper,IoExit } from "react-icons/io5";
+import logincss from '../styles/logincss.module.css'
 
 const Navbar = () => (
     <div>
-        <Link href="/"><a> Home </a></Link> |
-        <Link href="/register"><a> Register </a></Link>  |
-        <Link href="/login"><a> Login </a></Link> |
-        <Link href="/profile"><a> Profile </a></Link> | 
-        <Link href="/getConfig"><a> Config </a></Link> | 
-        <Link href="/presentproduct"><a> Amway Product </a></Link> | 
-        <Link href="/logout"><a> Logout </a></Link> 
+        <Link href="/"><a> <IoHome className = {logincss.size}/> Home </a></Link> |
+        <Link href="/register"><a> <IoAccessibility className = {logincss.size}/> Register </a></Link>  |
+        <Link href="/login"><a>  <IoLockClosed className = {logincss.size}/> Login </a></Link> |
+        <Link href="/profile"><a> <IoHappy className = {logincss.size}/> Profile </a></Link> | 
+        <Link href="/getConfig"><a> <IoNewspaper className = {logincss.size}/> Config </a></Link> | 
+        <Link href="/presentproduct"><a> <IoBagSharp  className = {logincss.size}/> Amway Product </a></Link> | 
+        <Link href="/logout"><a> <IoExit  className = {logincss.size}/> Logout </a></Link> 
     </div>
 )
 
